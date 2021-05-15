619922c57dd42cbc651e1f17b1474afd6df243a3
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-15 09:39:17 +0700

    Customized ui detail

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_apps/models/furniture.dart';
+import 'package:furniture_apps/theme.dart';
+
+class DetailPage extends StatelessWidget {
+  final Furniture furniture;
+  DetailPage({required this.furniture});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Image.asset(
+              furniture.imageAsset,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fill,
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 525,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE : TITLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  furniture.name,
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: 'by ',
+                                    style: greyTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                          text: furniture.producer,
+                                          style: blackTextStyle.copyWith(
+                                            fontSize: 16,
+                                          )),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.orange,
+                                ),
+                                Text(
+                                  '${furniture.rating}',
+                                  style: blackTextStyle,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      // NOTE : DESC
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Description',
+                          style: regulerTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          furniture.description,
+                          style: greyTextStyle.copyWith(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - (2 * edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              '\$${furniture.price}',
+                              style: blackTextStyle.copyWith(
+                                fontSize: 24,
+                              ),
+                            ),
+                            ElevatedButton(
+                              child: Text(
+                                'Buy Now',
+                                style: whiteTextStyle.copyWith(
+                                  fontSize: 18,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                primary: greenColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                              ),
+                              onPressed: () {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text("You pressed buy ;)"),
+                                ));
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+                vertical: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.arrow_back_ios),
+                  ),
+                  Icon(Icons.favorite_border),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
